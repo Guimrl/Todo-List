3d24213ca9eee5098fb65c703d719c080cd36738
Guilherme Amaral <88007295+Guimrl@users.noreply.github.com>
2023-06-19 17:19:12 -0300

    validação do status code

--- backend/routes/Route.php
+++ backend/routes/Route.php
@@ -2,6 +2,7 @@
 
 namespace backend\routes;
 
+use backend\utils\StatusCode;
 use backend\config\Connection;
 use backend\repository\TaskManager;
 
@@ -15,6 +16,7 @@ class Route
         $requestPath = $_SERVER['REQUEST_URI'];
 
         $connection = new Connection();
+        $statusCode = new StatusCode();
 
         if ($requestMethod === 'GET') {
             if ($requestPath === '/') {
@@ -22,9 +24,16 @@ class Route
             }
 
             if ($requestPath === '/task') {
-                $taskManager = new TaskManager($connection);
-                $tasks = $taskManager->getTasks();
-                echo json_encode($tasks);
+                try {
+                    $taskManager = new TaskManager($connection);
+                    $tasks = $taskManager->getTasks();
+                    http_response_code(200);
+                    $message = $statusCode->getStatusMessage(http_response_code());
+                    echo json_encode([$tasks, 'message' => $message, 'Status' => http_response_code()]);
+                } catch (\Throwable $th) {
+                    http_response_code(404);
+                    echo json_encode([$th->getMessage(), 'message' => $message . 'Endpoint não encontrado', 'Status' => http_response_code()]);
+                }
             }
         } else {
             http_response_code(404);
